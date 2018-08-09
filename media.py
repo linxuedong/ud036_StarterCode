143ee131a611abb4e22853431521701bcdcbc1c2
Dawn <xuedonglin1994@gmail.com>
2018-08-10 01:32:11 +0800

    doc: add movie docstring

--- media.py
+++ media.py
@@ -1,6 +1,15 @@
 class Movie():
-    """Data structure to store movie."""
-    def __init__(self, title, story_line, poster_image_url, trailer_youtube_url):
+    """Data structure to store movie.
+
+    Attributes:
+        title(str): The name of movie
+        story_line(str): The story line of movie
+        poster_image_url(str): The cover of movie
+        trailer_youtube_url(str): The link of this movie's trailer
+    """
+
+    def __init__(self, title, story_line, poster_image_url,
+                 trailer_youtube_url):
         self.title = title
         self.story_line = story_line
         self.poster_image_url = poster_image_url
